2846776926fc659e2867d0d5a30d4e4751f1efb1
Éloi Strée <eloistree@gmail.com>
2021-05-02 18:39:48 +0200

    Save: 05/02/2021 6:39 PM

---
+++ Runtime/UI/WebGL_Toggling.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class WebGL_Toggling : MonoBehaviour
+{
+    public UnityEvent m_webGLOn;
+    public UnityEvent m_webGLOff;
+    void Start()
+    {
+#if UNITY_WEBGL
+        
+        m_webGLOn.Invoke();
+#else
+
+        m_webGLOff.Invoke();
+#endif        
+    }
+
+}
